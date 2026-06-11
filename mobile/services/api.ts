5a24ecf4cef6e93eeead9ccd02a7c6f094f2db5f
Ramindu Sulakkana <ramindusulakkana08@gmail.com>
2026-06-11 13:15:44 +0530

    Update API URL for production

--- mobile/services/api.ts
+++ mobile/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://192.168.1.7:5000/api",
+  baseURL: "https://highwaygo-lk-app.onrender.com/api",
 });
 
 export default API;
